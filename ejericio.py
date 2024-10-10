9563b0b419ecffb0ea9c8d35195ac14ebae347f3
Mauricio Alejandro Campos Villalon <macamposv@ine.gob.cl>
2024-10-10 11:39:53 -0300

    version1

---
+++ ejericio.py
@@ -0,0 +1,16 @@
+n1 = 100
+n2 = 3
+sum = n1+n2
+rest = n1-n2
+divi = float(n1/n2)
+potencia1 = n1**n1
+potencia2 = n2**n2
+
+print("------------------Suma--------------------------")
+print(f'     El resultado de la suma es {sum} \n')
+print("------------------Resta--------------------------")
+print(f'     El resultado de la suma es {rest} \n')
+print("------------------Division-----------------------")
+print(f'     El resultado de la suma es {divi} \n')
+print("------------------Potencias-----------------------")
+print(f'     La raiz cuadrada del primer numero {n1} a suma es {divi} \n')
